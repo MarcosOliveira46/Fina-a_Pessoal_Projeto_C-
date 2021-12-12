9b7f549ba36ae9b60205787aad4fefdbffdbe6e9
MarcosOliveira46 <markiinjn@gmail.com>
2021-12-12 00:10:00 -0300

    Codigo funciona, se não procure por ==> se der erro retire

---
+++ Finance/Migrations/202112111745048_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿namespace Finance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Despesas",
+                c => new
+                    {
+                        Despesa_Id = c.Int(nullable: false, identity: true),
+                        Data_Despesa = c.DateTime(nullable: false),
+                        Despesa_Valor = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Despesa_Id);
+            
+            CreateTable(
+                "dbo.TipoDespesas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false),
+                        Descricao = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Despesas", t => t.Id)
+                .Index(t => t.Id);
+            
+            CreateTable(
+                "dbo.Receitas",
+                c => new
+                    {
+                        ReceitaId = c.Int(nullable: false, identity: true),
+                        Receita_Nome = c.Int(nullable: false),
+                        Data_Entrada = c.DateTime(nullable: false),
+                        Receita_Valor = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.ReceitaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TipoDespesas", "Id", "dbo.Despesas");
+            DropIndex("dbo.TipoDespesas", new[] { "Id" });
+            DropTable("dbo.Receitas");
+            DropTable("dbo.TipoDespesas");
+            DropTable("dbo.Despesas");
+        }
+    }
+}
---
+++ Finance/Models/Receita.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finance.Models
+{
+    public class Receita
+    {
+        [Key]
+        public int ReceitaId { get; set; }
+        [Display(Name = "Descrição")]
+        public TipoReceita Receita_Nome { get; set; }
+
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Display(Name = "Data")]
+        public DateTime Data_Entrada { get; set; }
+
+        [Display(Name = "Valor")]
+        public double Receita_Valor { get; set; }
+        public virtual TipoDespesa Tipo{ get; set; }
+    }
+
+    public enum TipoReceita
+    {
+        
+        Salario,
+        Extra
+    }
+}
